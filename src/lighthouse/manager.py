a1c051b3fa4802ab74f410711b5fcf3639bab8d4
fzaca <zaca03zaca@gmail.com>
2025-07-26 17:23:07 -0300

    feat: Add ProxyManager class skeleton

---
+++ src/lighthouse/manager.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from lighthouse.models import Lease, Proxy, ProxyFilters
+from lighthouse.storage import IStorage
+
+
+class ProxyManager:
+    """
+    Manages the lifecycle of proxies, including acquisition and release.
+
+    Parameters
+    ----------
+    storage : IStorage
+        The storage backend for proxy data.
+    """
+
+    def __init__(self, storage: IStorage):
+        self._storage = storage
+
+    def acquire_proxy(
+        self, pool_name: str, client_id: str, filters: Optional[ProxyFilters] = None
+    ) -> Optional[Proxy]:
+        """
+        Acquire a proxy from a given pool for a specific client.
+
+        Optionally matching given filters.
+
+        Parameters
+        ----------
+        pool_name : str
+            The name of the proxy pool.
+        client_id : str
+            The ID of the client acquiring the proxy.
+        filters : Optional[ProxyFilters], optional
+            Filtering criteria for selecting a proxy, by default None.
+
+        Returns
+        -------
+        Optional[Proxy]
+            The acquired proxy, or None if no suitable proxy is available.
+        """
+        pass
+
+    def release_proxy(self, lease: Lease) -> None:
+        """
+        Release a previously acquired proxy lease.
+
+        Parameters
+        ----------
+        lease : Lease
+            The lease to be released.
+        """
+        pass
